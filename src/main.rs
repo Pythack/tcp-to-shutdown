0fd44e8ff979bea7ffe44460dfe796d79c5c0fee
gmnsii <95436780+gmnsii@users.noreply.github.com>
2022-03-21 10:47:05 -0700

    Add OS detection and shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use core::panic;
+use std::env;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    const OS: &str = env::consts::OS;
+    let command = match OS {
+        "linux" => "poweroff",
+        "macos" => "poweroff",
+        "windows" => "shutdown /p",
+        _ => "",
+    };
+    run_command(command.to_string());
+}
+
+fn run_command(command: String) -> String {
+    if command.is_empty() {
+        panic!("failed to recognize your operating system")
+    }
+    let output = Command::new(command).output().expect("");
+    let stdout = String::from_utf8(output.stdout);
+
+    match stdout {
+        Err(_e) => String::from(""),
+        Ok(out) => out,
+    }
 }
